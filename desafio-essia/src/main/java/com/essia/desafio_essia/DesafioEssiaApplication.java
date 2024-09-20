49fb09dd9f2df07ce6964f6efbdcf7972246e7d2
JoaolimaDev <ozymandiasphp@gmail.com>
2024-09-20 15:37:00 -0300

    Primeiro commit

---
+++ desafio-essia/src/main/java/com/essia/desafio_essia/DesafioEssiaApplication.java
@@ -0,0 +1,13 @@
+package com.essia.desafio_essia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEssiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEssiaApplication.class, args);
+	}
+
+}
---
+++ desafio-essia/src/test/java/com/essia/desafio_essia/DesafioEssiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essia.desafio_essia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEssiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
