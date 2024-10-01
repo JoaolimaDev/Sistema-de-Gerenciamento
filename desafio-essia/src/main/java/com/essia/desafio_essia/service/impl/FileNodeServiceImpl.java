f6eb5f68a406ca0a49a97f2f74dfd854b2018dbf
JoaolimaDev <ozymandiasphp@gmail.com>
2024-10-01 18:05:02 -0300

    Alterações na camada service

--- desafio-essia/src/main/java/com/essia/desafio_essia/service/impl/FileNodeServiceImpl.java
+++ desafio-essia/src/main/java/com/essia/desafio_essia/service/impl/FileNodeServiceImpl.java
@@ -93,14 +93,11 @@ public class FileNodeServiceImpl implements FileNodeService {
         .orElseThrow(() -> new CustomException("Nenhum arquivo ou diretório encontrado para"+
         "o nome enviado!", HttpStatus.BAD_REQUEST));
 
-        if(fileNodePutRequest.newName() != name){
-
-            Optional<FileNode> findOptional = fileNodeRepository.findByname(fileNodePutRequest.newName());
-
-            if (findOptional.isPresent()) {
-                
-                throw new CustomException("O nome fornecido já está reservado: " + findOptional.get().getName(),
-                HttpStatus.BAD_REQUEST);
+        if (!fileNodePutRequest.newName().equals(name)) {
+            
+            Optional<FileNode> existingNodeOptional = fileNodeRepository.findByname(fileNodePutRequest.newName());
+            if (existingNodeOptional.isPresent()) {
+                throw new CustomException("O nome fornecido já está reservado: " + existingNodeOptional.get().getName(), HttpStatus.BAD_REQUEST);
             }
         }
 
